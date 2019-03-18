a892d4f4b37c42495647a7535935507d246cfa56
sávio <saviopf22@gmail.com>
2019-03-18 06:23:53 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// https://github.com/shama/letswritecode/tree/master/simple-p2p-with-webrtc
+// https://github.com/shama/letswritecode/tree/master/p2p-video-chat-webrtc
+
+const signalhub = require('signalhub')
+
+const hub = signalhub('my-app', ['http://localhost:8080'])
+
+
+hub.subscribe('update').on('data', (data) => {
+    document.getElementById('mensagem').textContent = data + '\n'
+    console.log(data)
+})
+
+document.getElementById('enviar').addEventListener('click', () => {
+    hub.broadcast('update', document.getElementById('suaMensagem').value)
+})
+
+/* //a cada segundo enviar dados para as pontas atualizarem 
+setInterval(() => {
+    hub.broadcast('update', window.location.hash)
+}, 1000)
+*/
