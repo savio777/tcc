ccf9a165552663753bc3cda88657f9e438b73a08
Sávio <saviopf22@gmail.com>
2019-10-14 09:09:43 -0300

    estilização do frontend e novas funções de verificação no backend

--- app/index.js
+++ app/index.js
@@ -2,24 +2,50 @@ const signalhub = require('signalhub')
 
 const hub = signalhub('my-app', ['http://localhost:8080'])
 
+const EC = require('elliptic').ec
+const ec = EC('secp256k1')
+
 const Block = require('../blockchain/block')
 const Blockchain = require('../blockchain/blockchain')
 
 const chain = new Blockchain()
 
 hub.subscribe('chain').on('data', (data) => {
-    chain.addBlock(new Block(new Date(), data.data, data.publicKey))
+  const keyPrivate = document.getElementById('privateKey').value
+
+  const keyLoggedPrivate = ec.keyFromPrivate(keyPrivate)
+
+  const keyLoggedPublic = keyLoggedPrivate.getPublic('hex')
+
+  if (keyLoggedPublic === data.publicKey) {
+    chain.addBlock(new Block(
+      new Date(),
+      {
+        description: data.description,
+        address: data.address,
+        amount: data.amount
+      }
+      , data.publicKey
+    ))
 
     document.getElementById('chain').textContent = JSON.stringify(chain, null, 2)
 
     document.getElementById('testChain').textContent = 'Corrente é válida: ' + chain.isValid()
+
+    console.log(chain)
+  }
+
+  document.getElementById('testKey').textContent = (keyLoggedPublic === data.publicKey) ?
+    `Chave é válida: ${true}` : `Chave é válida: ${false}`
 })
 
 document.getElementById('save').addEventListener('click', () => {
-    const data = {
-        data: document.getElementById('data').value,
-        publicKey: document.getElementById('publicKey').value
-    }
+  const data = {
+    description: document.getElementById('description').value,
+    address: document.getElementById('address').value,
+    amount: document.getElementById('amount').value,
+    publicKey: document.getElementById('publicKey').value
+  }
 
-    hub.broadcast('chain', data)
+  hub.broadcast('chain', data)
 })
